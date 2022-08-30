e44dee62bfd818cddcf870ce21c3aeadb73985a4
Fahmiiiiiiii <100488113+Fahmiiiiiiii@users.noreply.github.com>
2022-08-30 17:54:41 +0700

    Update wg.sh

--- wireguard/wg.sh
+++ wireguard/wg.sh
@@ -2,8 +2,8 @@
 # Debian 9 & 10 64bit
 # Ubuntu 18.04 & 20.04 bit
 # Centos 7 & 8 64bit 
-# By fromhell26
-# My Telegram : https://t.me/fromhell26
+# By FAHMISTORE
+# My Telegram : https://t.me/mailbintisukijem
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -18,18 +18,18 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/fromhell26/perizinan/main/ipvps.txt | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/Fahmiiiiiiii/perizinan/main/ipvps.txt | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
 echo -e "${NC}${RED}Permission Denied!${NC}";
 echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/fromhell26"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/mailbintisukijem"
 exit 0
 fi
 # ==================================================
 # Link Hosting Kalian
-fromhell26="raw.githubusercontent.com/fromhell26/scriptvps/main/wireguard"
+fromhell26="raw.githubusercontent.com/Fahmiiiiiiii/scriptvps/main/wireguard"
 
 # Check OS version
 if [[ -e /etc/debian_version ]]; then
@@ -114,9 +114,9 @@ WG_RUNNING=$?
 
 # Tambahan
 cd /usr/bin
-wget -O addwg "https://${fromhell26}/addwg.sh"
-wget -O delwg "https://${fromhell26}/delwg.sh"
-wget -O renewwg "https://${fromhell26}/renewwg.sh"
+wget -O addwg "https://${Fahmiiiiiiii}/addwg.sh"
+wget -O delwg "https://${Fahmiiiiiiii}/delwg.sh"
+wget -O renewwg "https://${Fahmiiiiiiii}/renewwg.sh"
 chmod +x addwg
 chmod +x delwg
 chmod +x renewwg
