3989be9905a1dde7b7a88e3ae047120fd738bfe5
Fahmiiiiiiii <100488113+Fahmiiiiiiii@users.noreply.github.com>
2022-09-01 20:10:26 +0700

    Update addl2tp.sh

--- ipsec/addl2tp.sh
+++ ipsec/addl2tp.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/fromhell26/perizinan/main/ipvps.txt | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/Fahmiiiiiiii/perizinan/main/ipvps.txt | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
